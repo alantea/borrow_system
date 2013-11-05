dc208a7ce20e916b40c3ff37154447d6cb77ab5a
Ho chia lun <hcl100u@csie1.cs.ccu.edu.tw>
2013-11-06 00:32:24 +0800

    First commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+
+$(function() {
+	$( "#datepicker" ).datepicker(
+	{
+		altField: "#alternate",
+		altFormat: "yy-mm-dd" ,
+		onSelect: function()
+		{
+			var refresh="日期 : " + getday();
+			$( ".CurrDate" ).text( refresh );
+		}
+	});
+});
+
+function getday()
+{
+	var dat = $( "#datepicker" ).datepicker('getDate' );
+	var year = dat.getFullYear() + "-";
+	var month = ( dat.getMonth() + 1) + "-";
+	var day = dat.getDate();
+	return year + month + day;
+}
+
+jQuery(document).ready(function(){
+	var refresh="日期 : " + getday();
+	$( ".CurrDate" ).text( refresh );
+});
