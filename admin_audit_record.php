4b01d2891fd420a811da9de710b04f35822e3ed5
Poring <hcl100u@cs.ccu.edu.tw>
2013-12-30 23:38:42 +0800

    add admin audit record detail

--- admin_audit_record.php
+++ admin_audit_record.php
@@ -23,6 +23,17 @@
 	<link href="css/jquery-ui.min.css" rel="stylesheet" />
 	<link href="css/user_index.css" rel="stylesheet" />
 	<link href="css/theme.bootstrap.css" rel="stylesheet" />
+	<style>
+		.permit{
+			color: green;
+		}
+		.deny{
+			color: red;
+		}
+		.wait{
+			color: #0000CC;
+		}
+	</style>
 
 	<!-- Javascript --!>
 	<script src="js/jquery-1.10.2.min.js"></script>
@@ -60,6 +71,8 @@
 						<th>申請人</th>
 						<th>連絡電話</th>
 						<th data-sorter="false">參加人數</th>
+						<th>審核結果</th>
+						<th data-sorter="false">詳細資料</th>
 					</tr>
 				</thead>
 				<tbody>
@@ -67,15 +80,16 @@
 		<?php
 			require("config/config.php");
 	
-			$stmt = $mysqli->prepare("SELECT id,date,time,loc,club,pm,name,phone,attend,bdate,btime,admin_result
+			$stmt = $mysqli->prepare("SELECT id,date,time,loc,club,pm,name,phone,attend,bdate,btime,admin_name,
+											 admin_date,admin_result,admin_reason
 	                          FROM dorm_list WHERE admin_result != ? ORDER By date");
 	
-	$res = "Waiting";
+	$res = "wait";
 	$stmt->bind_param("s", $res);
 	
 	$stmt->execute();
 
-	$stmt->bind_result($id,$date,$time,$loc,$club,$pm,$name,$phone,$attend,$bdate,$btime,$ad_result);
+	$stmt->bind_result($id,$date,$time,$loc,$club,$pm,$name,$phone,$attend,$bdate,$btime,$ad_name,$ad_date,$ad_result,$ad_reason);
 
 	$borrowed = false;
 				
@@ -105,8 +119,101 @@
 		$list .= $club . "</td><td>";
 		$list .= $pm . "</td><td>";
 		$list .= $phone . "</td><td>";
-		$list .= $attend . "</td><tr>";
+		$list .= $attend . "</td>";
+		$list .= '<td class="' . $ad_result . '">';
+		if( $ad_result == 'permit' )
+		{
+			$list .= '通過';
+		}
+		else if( $ad_result == 'deny' )
+		{
+			$list .= '不通過';
+		}
+		$list .= "</td><td>";
 		$borrowed = true;
+		$list .= '<div class="btn btn-default" data-toggle="modal" 
+					   data-target="#myModal' . $i . '">Detail</div>' .
+'<div class="modal fade" id="myModal' . $i . '" tabindex="-1" role="dialog" aria-labelledby="myModalLabel' . $i . '" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title" id="myModalLabel">Detail</h4>
+      </div>
+      <div class="modal-body">
+			<div class="row">
+				<label for="date" class="col-sm-3">日期</label>
+				<div class="col-sm-5">' . $date . '</div>
+			</div>
+			<div class="row">
+				<label for="date" class="col-sm-3">時段</label>
+				<div class="col-sm-5">' . $time . '</div>
+			</div>
+			<div class="row">
+				<label for="date" class="col-sm-3">地點</label>
+				<div class="col-sm-5">' . $loc . '</div>
+			</div>
+			<div class="row">
+				<label for="date" class="col-sm-3">社團</label>
+				<div class="col-sm-5">' . $club . '</div>
+			</div>
+			<div class="row">
+				<label for="date" class="col-sm-3">申請人</label>
+				<div class="col-sm-5">' . $pm . '</div>
+			</div>
+			<div class="row">
+				<label for="date" class="col-sm-3">活動名稱</label>
+				<div class="col-sm-5">' . $name . '</div>
+			</div>
+			<div class="row">
+				<label for="date" class="col-sm-3">連絡電話</label>
+				<div class="col-sm-5">' . $phone . '</div>
+			</div>
+			<div class="row">
+				<label for="date" class="col-sm-3">參加人數</label>
+				<div class="col-sm-5">' . $attend . '</div>
+			</div>
+			<div class="row">
+				<label for="date" class="col-sm-3">借用時間</label>
+				<div class="col-sm-5">' . $bdate . ' ' . $btime . '</div>
+			</div>
+			<div class="row">
+				<label for="date" class="col-sm-3">借用結果</label>
+				<div class="col-sm-5">';
+				if( $ad_result == 'wait' )
+				{
+					$list .= '處理中';
+				}
+				else if( $ad_result == 'permit' )
+				{
+					$list .= '通過';
+				}
+				else if( $ad_result == 'deny' )
+				{
+					$list .= '不通過';
+				}
+
+				$list .= '</div>
+			</div>
+			<div class="row">
+				<label for="date" class="col-sm-3">審核人</label>
+				<div class="col-sm-5">' . $ad_name . '</div>
+			</div>
+			<div class="row">
+				<label for="date" class="col-sm-3">審核時間</label>
+				<div class="col-sm-5">' . $ad_date . '</div>
+			</div>
+			<div class="row">
+				<label for="date" class="col-sm-3">不借用原因</label>
+				<div class="col-sm-5">' . $ad_reason . '</div>
+			</div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->' . '</td></tr>';
 
 		echo $list;
 	}
