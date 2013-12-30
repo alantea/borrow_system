2cf5845baad23b5b26efeafa7d0120699870ba0d
Mytest <test@email.com>
2013-12-30 10:36:11 +0800

    change user_rules

--- user_rules.php
+++ user_rules.php
@@ -54,13 +54,21 @@
 			<br />
 			<div class="panel panel-default">
 				<div class="panel-heading">
-					借用注意事項
+					學士班宿舍場地申請登記注意事項
 				</div>
 				<div class="panel-body">
 				<ol type='1'>
-					<li>活動當日起10天起，始可申請場地</li>
-					<li>借用時間為三小時之內(含)</li>
-					<li>同一團體一週內(週日起至週六止)最多僅能申請2場次</li>
+					<li>申請日期 : 活動當日起10天起，始可申請場地。</li>
+					<li>申請時間 : 每日08時至22時止。</li>
+					<li>場地開放使用時間 : 每日08時起至23時止。</li>
+					<li>場地容量分配 :<br>
+						30人以下團體限用小AB1、小CB1交誼廳。<br>
+					    60人以上團體始可直接借用大AB1、大CB1交誼廳。<br>
+						30人至60人之團體，視場地空餘情況及活動需要核辦。<br>
+						大AB1、大CB1交誼廳以容納80人為標準，50人以下之團體借用大交誼廳時，須同意許其他社團合併使用，始予核准。
+					</li>
+					<li>場地借用次數限制 : 同一團體一週內(週日起至週六止)最多僅能申請2場次。</li>
+					<li>基於安全考量，宿舍區內禁止舉辦各類型式炊煮活動。</li>
 					<li>
 						若需特殊借閱，請點選<a href="user_add_spc.php">此處</a>
 					</li>
@@ -68,11 +76,10 @@
 				</div>
 			</div>
 			<form class="form-horizontal" role="form" action="user_add.php" method="POST">
-				<div class="form-group">
-					<div class="col-sm-2">
-						<input type="checkbox" class="form-control" id="agree" name="agree" required>
-					</div>
-					<label for="name" class="col-sm-4 control-label">我同意balabala</label>
+				<div class = "checkbox" style = "margin-bottom:10px">
+					<label>
+					<input type="checkbox" id="agree" name="agree" required>我已詳閱以上條款，並同意遵守配合。
+					</label>
 				</div>
 				<button type="submit" class="btn btn-default" id="nextstep">
 					下一步	<span class="glyphicon glyphicon-chevron-right"></span>
